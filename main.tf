63f9fc4669b5cdad4d3d8efc59bf9ac5b8b82595
Heinz Schmidt <heinz.schmidt@wdtrust.org.uk>
2022-12-08 11:05:26 +0000

    Reset

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "REPLACE_ME_HERE"
 
     workspaces {
       name = "REPLACE_ME"
